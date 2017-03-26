37ab98a3b5e6ea4eb9143eef7df40561a9519d31
Damon Zwolinski <damonz@codeswim.com>
2017-03-26 16:44:23 -0700

    REDO: Task 9. Make documentation test pass.

--- tests/documentation/README.md.php
+++ tests/documentation/README.md.php
@@ -9,9 +9,16 @@ return [
 	$start_idx = strpos( $readme_md, '## Code Example' ) + strlen( '## Code Example' );
 	$stop_idx = strpos( $readme_md, '## Motivation' );
 	$code_example = substr( $readme_md, $start_idx, $stop_idx - $start_idx );
-	$code_example = str_replace( "require 'src/tdop.php';", '', $code_example );
-	$code_example = str_replace( 'echo ', 'return ', $code_example );
+
+	$replacements = [
+		"require 'src/tdop.php';" => '',
+		'echo ' => 'return ',
+		'```php' => '',
+		'```' => '',
+	];
+	$code_example = str_replace( array_keys( $replacements ), array_values( $replacements ), $code_example );
 	$json = eval( $code_example );
+
 	$result = strpos( $json, '"value": "expression"' );
 	return (($result !== false) and (0 < $result));
 },
